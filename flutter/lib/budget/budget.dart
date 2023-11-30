593564585e529e5cadf10e66914fbbf5e36fac4d
R-LS <shuyi.lin.2020@scis.smu.edu.sg>
2023-11-30 15:02:34 -0500

    added donut chart to budget page

--- flutter/lib/budget/budget.dart
+++ flutter/lib/budget/budget.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class BudgetPage extends StatefulWidget {
   const BudgetPage({super.key});
@@ -11,27 +13,56 @@ class BudgetPage extends StatefulWidget {
   @override
   State<BudgetPage> createState() => _BudgetPageState();
 }
+class _ChartData {
+  _ChartData(this.x, this.y);
 
+  final String x;
+  final double y;
+}
 class _BudgetPageState extends State<BudgetPage> {
+  late List<_ChartData> data;
+  late TooltipBehavior _tooltip;
+
+  @override
+  void initState() {
+    super.initState();
+    data = [
+      _ChartData('David', 25),
+      _ChartData('Steve', 38),
+      _ChartData('Jack', 34),
+      _ChartData('Others', 52)
+    ];
+    _tooltip = TooltipBehavior(enable: true);
+  }
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-        appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(
-                "Budget",
-                style: TextStyle(
-                  fontSize: 24,
-                  fontFamily: GoogleFonts.openSans().fontFamily,
-                  fontWeight: FontWeight.bold),
+      appBar: AppBar(
+        title: const Text('Syncfusion Flutter chart'),
+      ),
+      body: Stack(
+        children: [
+          SfCircularChart(
+            tooltipBehavior: _tooltip,
+            series: <CircularSeries<dynamic, dynamic>>[
+              DoughnutSeries<_ChartData, String>(
+                dataSource: data,
+                xValueMapper: (_ChartData data, _) => data.x,
+                yValueMapper: (_ChartData data, _) => data.y,
+                innerRadius: '80%',
+                name: 'Gold',
+              ),
+            ],
+          ),
+          Center(
+            child: Text(
+              'xxxx spent this month, 80% of budget spent',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
             ),
-        ),
-      body: Text("Budget Page")
+          ),
+        ],
+      ),
     );
   }
 }
